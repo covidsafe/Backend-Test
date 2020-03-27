2fca887be3ef2f2bc0e1b08abb7451251b99dbc3
Mutinifni <patelp1@cs.washington.edu>
2020-03-27 16:20:54 -0700

    Implement basic registerUser and sendInfectedLogs
    Signed-off-by: Pratyush Patel <patelp1@cs.washington.edu>

--- covidsafe/grpc_service.py
+++ covidsafe/grpc_service.py
@@ -3,16 +3,52 @@ from proto.covidsafe_pb2 import *
 from proto.covidsafe_pb2_grpc import *
 
 import grpc
+import pymongo
+import mongo_config
+import logging
+
+client = pymongo.MongoClient(mongo_config.URI)
+database = client.database
 
 class GrpcService(CovidSafeServerServicer):
+    """grpc service to handle client rpcs"""
+
     def registerUser(self, request, context):
         print("Called registerUser")
-        return Registered(success=False)
+        try:
+            collection = database.registration
+            user = {
+                    "phone": request.phone,
+                    "key": request.key
+                   }
+            collection.insert_one(user)
+            return Registered(success=True)
+        except Exception as e:
+            print(e)
+            return Registered(success=False)
 
     def sendInfectedLogs(self, request_iterator, context):
+        collection = database.blt_logs
         for log in request_iterator:
-            pass
-        return AddedLogs(success=False)
+            print(log.type)
+            if log.type == Log.LogType.BLT:
+                record = {
+                          "timestamp": log.timestamp.ToJsonString(),
+                          "uuid": log.bltResult.uuid.decode(),
+                          "blt_name": log.bltResult.name
+                         }
+                collection.insert_one(record)
+            elif log.type == Log.LogType.GPS:
+                record = {
+                          "timestamp": log.timestamp.ToJsonString(),
+                          "latitude": log.GPSCoordinate.latitude,
+                          "longitude": log.GPSCoordinate.longitude
+                         }
+                collection.insert_one(record)
+            else:
+                print
+
+        return AddedLogs(success=True)
 
     def getBLTContactLogs(self, request_iterator, context):
         for key in request_iterator:
--- covidsafe/mongo_config.py
+++ covidsafe/mongo_config.py
@@ -1,16 +1,15 @@
-"""
-MongoDB client endpoint
-"""
-
 import os
-import pymongo
-
 
 DB_NAME = "covidsafe-db"
+
 HOST = "covidsafe-db.mongo.cosmos.azure.com"
+
 PORT = 10255
+
 USERNAME = os.getenv("MONGO_USERNAME")
+
 PASSWORD = os.getenv("MONGO_PASSWORD")
+
 ARGS = "ssl=true&" \
        "retrywrites=false&" \
        "maxIdleTimeMS=12000&" \
@@ -24,14 +23,3 @@ URI = "mongodb://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}?{ARGS}".format(
     DB_NAME=DB_NAME,
     ARGS=ARGS
 )
-
-
-def start_client():
-    """Sends mongodb queries"""
-    client = pymongo.MongoClient(URI)
-    for database in client.list_databases():
-        print(database)
-
-
-if __name__ == "__main__":
-    start_client()
