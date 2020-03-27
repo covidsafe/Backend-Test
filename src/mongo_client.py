657bf6473540120d5646ecfc842ac2a8f6d5e13a
Sudheesh Singanamalla <sudheesh@cs.washington.edu>
2020-03-26 23:10:01 -0700

    Requirement for grpc_tools and grpc-io-tools for proto, rpc stub generation
    Signed-off-by: Sudheesh Singanamalla <sudheesh@cs.washington.edu>

--- src/mongo_client.py
+++ src/mongo_client.py
@@ -17,14 +17,21 @@ ARGS = "ssl=true&\
         maxIdleTimeMS=12000&\
         appName=@covidsafe-db@"
 
-URI = f"mongodb://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}?{ARGS}"
+URI = "mongodb://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}?{ARGS}".format(
+    USERNAME=USERNAME,
+    PASSWORD=PASSWORD,
+    HOST=HOST,
+    PORT=PORT,
+    DB_NAME=DB_NAME,
+    ARGS=ARGS
+)
 
 
 def start_client():
     """Sends mongodb queries"""
     client = pymongo.MongoClient(URI)
-    for db in client.list_databases():
-        print(db)
+    for database in client.list_databases():
+        print(database)
 
 
 if __name__ == "__main__":
