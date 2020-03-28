a48a68eff3358c38182c7fed27e8f827de2218c6
Mutinifni <patelp1@cs.washington.edu>
2020-03-27 17:16:16 -0700

    Update proto and add simple logging to grpc server
    Signed-off-by: Pratyush Patel <patelp1@cs.washington.edu>

--- covidsafe/grpc_service.py
+++ covidsafe/grpc_service.py
@@ -7,14 +7,30 @@ import pymongo
 import mongo_config
 import logging
 
+# set up logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+
+# set up connection to mongoDB
 client = pymongo.MongoClient(mongo_config.URI)
 database = client.database
 
+# grpc service handler
 class GrpcService(CovidSafeServerServicer):
     """grpc service to handle client rpcs"""
 
     def registerUser(self, request, context):
-        print("Called registerUser")
+        logger.debug("Received registerUser() with ({phone},{key})".format(
+            phone=request.phone,
+            key=request.key))
         try:
             collection = database.registration
             user = {
@@ -24,33 +40,34 @@ class GrpcService(CovidSafeServerServicer):
             collection.insert_one(user)
             return Registered(success=True)
         except Exception as e:
-            print(e)
+            logger.error(e)
             return Registered(success=False)
 
     def sendInfectedLogs(self, request_iterator, context):
+        logger.debug("Received sendInfectedLogs()")
         collection = database.blt_logs
         for log in request_iterator:
             print(log.type)
             if log.type == Log.LogType.BLT:
                 record = {
-                          "timestamp": log.timestamp.ToJsonString(),
+                          "timestamp": log.timestamp,
                           "uuid": log.bltResult.uuid.decode(),
                           "blt_name": log.bltResult.name
                          }
                 collection.insert_one(record)
             elif log.type == Log.LogType.GPS:
                 record = {
-                          "timestamp": log.timestamp.ToJsonString(),
+                          "timestamp": log.timestamp,
                           "latitude": log.GPSCoordinate.latitude,
                           "longitude": log.GPSCoordinate.longitude
                          }
                 collection.insert_one(record)
             else:
-                print
-
+                logger.error("Unknown LogType in sendInfectedLogs()")
         return AddedLogs(success=True)
 
     def getBLTContactLogs(self, request_iterator, context):
+        logger.debug("Received getBLTContactLogs()")
         for key in request_iterator:
             pass
         yield BLTContactLog()
